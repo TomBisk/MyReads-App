8037a51eb7b8247ed58636d27908eabd54038aba
Tomasz Bochenek <bochenekt@gmail.com>
2018-08-15 23:36:15 +0200

    feat: add routing for main a search pages
    How it works?
    - when the + button on main page (BookShelf) is clicked, the search
    page is displayed (URL is /search);
    -when the back button on search page (SearchBook) or back button in browser is cliked, the main page is displayed (URL is /)

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class BookShelf extends Component {
 	
@@ -163,7 +164,9 @@ render() {
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <Link 
+								to="/search"
+							>Add a book</Link>
             </div>
           </div>
 	
