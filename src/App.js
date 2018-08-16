2a05411f9c5948db249ccd3512ee8fdace184ff4
Tomasz Bochenek <bochenekt@gmail.com>
2018-08-16 21:52:25 +0200

    feat: add BookAPI getAll method to get books and update Book to coorect display book details

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
-
-import Book from './Book'
+//import Book from './Book'
 import BookShelf from './BookShelf'
 import SearchBook from './SearchBook'
 
 
-const bookStore = [
-	{
+
+class BooksApp extends React.Component {
+  state = {
+ 		bookStore : [
+/*	{
 		"title" : "The Hobbit",
 		"author" : "J.R.R. Tolkien",
 		"coverURL" : "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api",
@@ -23,30 +25,42 @@ const bookStore = [
 		"title" : "The Adventures of Tom Sawyer",
 		"author" : "Mark Twain",
 		"coverURL" : "http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api",
+	},
+	{
+		"title" : "Ender's Game",
+		"author" : "Orson Scott Card",
+		"coverURL" : "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
+	},
+	{
+		"title" : "To Kill a Mockingbird",
+		"author" : "Harper Lee",
+		"coverURL" : "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
+	},
+	{
+		"title" : "1776",
+		"author" : "David McCullough",
+		"coverURL" : "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
+	},*/
+	]
+}
+
+	componentDidMount() {
+		BooksAPI.getAll().then((bookStore) => {
+			this.setState({ bookStore })
+		})
 	}
-]
 
-
-class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-//    showSearchPage: false
-		
-  }
 
   render() {
     return (
       <div className="app">
 				<Route exact path="/" render={() => (
-					<BookShelf bookStor={bookStore}/>		
+					<BookShelf bookStore={this.state.bookStore}/>		
 				)}/> 
 				
-				<Route path="/search" component={SearchBook}/>
+				<Route path="/search" render={() => (
+					<SearchBook bookStore={this.state.bookStore}/>														
+				)}/>
       </div>
     )
   }
--- src/Book.js
+++ src/Book.js
@@ -6,7 +6,7 @@ class Book extends Component {
 		return (
 			<div className="book">
 				<div className="book-top">
-					<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.coverURL})`}}></div>
+				<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})`}}></div>
 					<div className="book-shelf-changer">
 						<select>
 							<option value="move" disabled>Move to...</option>
@@ -18,10 +18,11 @@ class Book extends Component {
 					</div>
 				</div>
 				<div className="book-title">{this.props.book.title}</div>
-				<div className="book-authors">{this.props.book.author}</div>
+				<div className="book-authors">{this.props.book.authors}</div>
 			</div>
 		)
 	}
 }
+
 
 export default Book
