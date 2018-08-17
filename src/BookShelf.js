7eb9704bd5e6e71699a1dbb39c5117afefca5fde
Tomasz Bochenek <bochenekt@gmail.com>
2018-08-17 22:07:59 +0200

    recator: refactor BookShelf component to stateless functional component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -2,71 +2,68 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import Book from './Book'
 
-class BookShelf extends Component {
-		
-	render() {
-		return (
-			<div className="list-books">
-				<div className="list-books-title">
-					<h1>MyReads</h1>
-				</div>
-				<div className="list-books-content">
-					<div>
-						<div className="bookshelf">
-							<h2 className="bookshelf-title">Currently Reading</h2>
-							<div className="bookshelf-books">
-								<ol className="books-grid">
-									{this.props.myBooks.filter(book => book.shelf === "currentlyReading").map((book) => (
-			
-										<li key={book.id}>
-											<Book book={book} moveBook={this.props.moveBook} 
-			currentShelf="currentlyReading"/>
-										</li>
-									))}
-								</ol>
-							</div>
-						</div>
 
-						<div className="bookshelf">
-							<h2 className="bookshelf-title">Want to Read</h2>
-							<div className="bookshelf-books">
-								<ol className="books-grid">
-									{this.props.myBooks.filter(book => book.shelf === "wantToRead").map((book) => (
-			
-										<li key={book.id}>
-											<Book book={book} moveBook={this.props.moveBook}
-currentShelf="wantToRead"/>
-										</li>
-									))}
-								</ol>
-							</div>
-						</div>
-
-						<div className="bookshelf">
-							<h2 className="bookshelf-title">Read</h2>
-							<div className="bookshelf-books">
-								<ol className="books-grid">
-									{this.props.myBooks.filter(book => book.shelf === "read").map((book) => (
-			
-										<li key={book.id}>
-											<Book book={book} moveBook={this.props.moveBook}
-currentShelf="read" />
-										</li>
-									))}
-								</ol>
-							</div>
-						</div>
+const BookShelf = (props) => (	
+	<div className="list-books">
+		<div className="list-books-title">
+			<h1>MyReads</h1>
+		</div>
+		<div className="list-books-content">
+			<div>
+				<div className="bookshelf">
+					<h2 className="bookshelf-title">Currently Reading</h2>
+					<div className="bookshelf-books">
+						<ol className="books-grid">
+							{props.myBooks.filter(book => book.shelf === "currentlyReading").map((book) => (
+								<li key={book.id}>
+									<Book 
+										book={book} 
+										moveBook={props.moveBook} 
+										currentShelf="currentlyReading"
+									/>
+								</li>
+							))}
+						</ol>
 					</div>
 				</div>
-
-				<div className="open-search">
-					<Link 
-						to="/search"
-					>Add a book</Link>
+				<div className="bookshelf">
+					<h2 className="bookshelf-title">Want to Read</h2>
+					<div className="bookshelf-books">
+						<ol className="books-grid">
+							{props.myBooks.filter(book => book.shelf === "wantToRead").map((book) => (
+								<li key={book.id}>
+									<Book 
+										book={book} 
+										moveBook={props.moveBook}
+										currentShelf="wantToRead"
+									/>
+								</li>
+							))}
+						</ol>
+					</div>
+				</div>
+				<div className="bookshelf">
+					<h2 className="bookshelf-title">Read</h2>
+					<div className="bookshelf-books">
+						<ol className="books-grid">
+							{props.myBooks.filter(book => book.shelf === "read").map((book) => (
+								<li key={book.id}>
+									<Book 
+										book={book} 
+										moveBook={props.moveBook}
+										currentShelf="read" 
+									/>
+								</li>
+							))}
+						</ol>
+					</div>
 				</div>
 			</div>
-		)
-	}
-}
+		</div>
+		<div className="open-search">
+			<Link to="/search">Add a book</Link>
+		</div>
+	</div>
+)
 
 export default BookShelf
